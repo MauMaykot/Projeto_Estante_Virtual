39bda60a1beb1575d1b80663741dd7b69ecfb690
maumaykot <maumaykot@gmail.com>
2021-02-07 11:46:12 -0300

    Versao_Final(?)

--- crawler.py
+++ crawler.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
 from datetime import datetime
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 driver = webdriver.Chrome(executable_path=r"C:\Program Files (x86)/chromedriver.exe")
 driver.maximize_window()
@@ -20,8 +22,9 @@ wait = WebDriverWait(driver, 20)
 
 links_biografias = []
 links_biografias_especificas = []
-
 nome_classe_botao_next = []
+nome_classe_botao_especifico_next = []
+
 #url = driver.current_url
 
 #pegar os links das biografias e mudar de pág.#
@@ -32,7 +35,7 @@ while True:
 
     for i in links:
         links_biografias.append(i.get_attribute('href'))    
-        #print(links_biografias)
+        print(links_biografias)
 
     lista = driver.find_elements_by_xpath('//nav//ul[@class="pagination"]//li')
     
@@ -42,7 +45,7 @@ while True:
     try:
         botao = driver.find_element_by_xpath('//a[@aria-label="próxima"]')
         botao.click()
-        sleep(5)
+        sleep(2)
 
         if nome_classe_botao_next[-1] == "m-next":
             pass
@@ -51,29 +54,49 @@ while True:
     except:
         pass
 
+
+
 #pegar os links da biografia específica e mudar de pág.#
 
 for linkbylink_biografias in links_biografias:
     driver.get(linkbylink_biografias)
+
+    tamanho_pag = driver.execute_script("return document.body.scrollHeight")
 
     while True:
         
         dados = driver.find_elements_by_xpath('//a[@aria-label="exemplar"]')
         
         for i in dados:
-
             links_biografias_especificas.append(i.get_attribute('href'))       
-            print(links_biografias_especificas)
+            #print(links_biografias_especificas)
 
-            for i in range(10):
-                driver.execute_script("window.scrollBy(0,500)","")
-                sleep(1)
-            try:
-                botao = driver.find_element_by_xpath('//a[@aria-label="proxima"]')
-                botao.click()
-                sleep(8)
-            except:
-                pass
+        for i in range(10):
+            driver.execute_script("window.scrollBy(0, 500)","")
+            sleep(1)
+
+        lista = driver.find_elements_by_xpath('//div[@class="paginador pg-footer"]//a[@aria-label="proxima"]')
+        
+        for i in lista:
+            nome_classe_botao_especifico_next.append(i.get_attribute('class'))
+            print(nome_classe_botao_especifico_next)
+            print(nome_classe_botao_especifico_next[-1])
+
+        if nome_classe_botao_especifico_next[-1] == "next nuxt-link-exact-active nuxt-link-active desativado":
+            break
+        else:
+            pass    
+
+        try:
+
+            botao = driver.find_element_by_xpath('//a[@aria-label="proxima"]')
+            botao.click()
+            sleep(2)        
+
+            print(nome_classe_botao_especifico_next[-1])
+
+        except:
+            pass
 
 # fazer a coleta dos dados
 
